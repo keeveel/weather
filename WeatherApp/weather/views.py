c64e6997d3c6ff5db785503157de3671f24d286a
Azamat.Jumagulov <djuma94@gmail.com>
2022-08-08 15:51:51 +0600

    все доделано, остается перенести

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,5 +1,7 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 
 # 12b81732ac2c1769723ed335b035c02e - key weatherapp
@@ -7,14 +9,27 @@ from django.shortcuts import render
 def index(request):
     appid = '12b81732ac2c1769723ed335b035c02e'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Moscow'
-    response = requests.get(url.format(city)).json()
-    city_info = {
-        'city': city,
-        'temp': response['main']['temp'],
-        'icon': response['weather'][0]['icon']
-    }
 
-    context = {'info': city_info}
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        response = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': response['main']['temp'],
+            'icon': response['weather'][0]['icon']
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form':form}
 
     return render(request, 'weather/index.html', context)
